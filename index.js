f1278ae12330de236d687ba99581b2db33eaa927
asynchronous-widget-3412 <rikeiwilliams@gmail.com>
2019-07-30 16:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,5 +61,11 @@ for (let i = 0; i < cart.length; i++) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  var total = total();
+  if(cardNumber) {
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`;
+  } else {
+    return `Sorry, we don't have a credit card on file for you`;
+  }
   
 }
